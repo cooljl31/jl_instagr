e3b34b3e4ebc1d88ac92b1c7cc8ee831cc6d73b8
jim lesperance <contact@jimlesperance.com>
2017-03-13 08:21:16 +0100

    add image path

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,7 +1,9 @@
 class Pic < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }, default_url: "/images/:style/missing.png",
+                                  :path => ":rails_root/asset/images/system/:attachment/:username.:extension",
+                                  :url  => "/:attachment/:username.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   acts_as_votable
 end
