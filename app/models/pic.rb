4516cdc7af3401a1f78aee0dc64732dbe970ea4d
jim lesperance <contact@jimlesperance.com>
2017-03-13 08:32:38 +0100

    fix image path

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,9 +1,10 @@
 class Pic < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :image, styles: { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }, default_url: "/images/:style/missing.png",
-                                  :path => ":rails_root/asset/images/system/:attachment/:username.:extension",
-                                  :url  => "/:attachment/:username.:extension"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, styles: { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" },
+                                  default_url: "/images/:style/missing.png",
+                                  path: ":rails_root/public/images/:id/:style/:filename",
+                                  url: "/images/:id/:style/:filename"
+ validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   acts_as_votable
 end
